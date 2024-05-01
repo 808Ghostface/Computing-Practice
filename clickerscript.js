d932aa4c6262651e2af50ea3bb67d629c9d77157
808Ghostface <90937214+808Ghostface@users.noreply.github.com>
2024-05-01 08:54:45 +0100

    update clickerscript.js
    trying to get timer to work

--- clickerscript.js
+++ clickerscript.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Variables
-    let minutes = 00;
-    let seconds = 00;
     let clickCount = 0;
     let timer;
     let timeLeft;
@@ -11,12 +9,16 @@ document.addEventListener("DOMContentLoaded", function () {
     // Elements
     const amendMinutes = document.getElementById("minutes");
     const amendSeconds = document.getElementById("seconds");
+  ////  const set15 = document.getElementById("set-15");
+  //  const set30 = document.getElementById("set-30");
+  //  const set60 = document.getElementById("set-60");
+
     const clickCounter = document.getElementById("click-counter");
-    const clickButton = document.getElementById("click-button");
+    const clickButton = document.getElementById("click-btn");
     const multiplierDisplay = document.getElementById("multiplier-display");
     const setTimer = document.getElementById("set-timer");
     const startTimer = document.getElementById("start-timer");
-    const dropDown = document.getElementById("timer-dropdown");
+
 
     clickButton.addEventListener("click", function () {
        clickCount++;
@@ -24,21 +26,41 @@ document.addEventListener("DOMContentLoaded", function () {
    });
 
    setTimer.addEventListener("click",function() {
+
      showTime();
    });
 
    function showTime() {
-
-minutes = Math.floor(time / 60);
-seconds = Math.floor(time / 60);
+    selectTime = document.querySelector('#timer-dropdown');
+    minutes = selectTime.value;
+    document.querySelector('.minutes').textContent = minutes/60;
+    if (document.querySelector('.minutes').textContent < 10){
+      document.querySelector('.minutes').textContent = "0" + document.querySelector('.minutes').textContent;
+      }
    }
 
- startTimer.addEventListener("click",function() {
+   function goTime() {
+     let countDown = document.getElementById("seconds");
+     if (document.querySelector('.minutes').textContent < 61)
+     {
+       countDown = 59
+     }
+const timer = setInterval(function() {
+  countDown--;
+  console.log(countdowns);
+  if (countDown === 0) {
+    clearInterval(timer);
+    console.log("Time's up!");
+  }
+}, 1000);
+}
 
+ startTimer.addEventListener("click",function() {
+goTime();
  });
 
  window.addEventListener("beforeunload", function () {
      clearInterval(timerInterval);
  });
-
 });
+
