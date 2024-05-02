e43e584cda872fe4ecf78b55e4fb0f4e0d514d6d
808Ghostface <90937214+808Ghostface@users.noreply.github.com>
2024-05-02 11:56:22 +0100

    Update clickerscript.js
    final touch

--- clickerscript.js
+++ clickerscript.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Variables
     let clickCount = 0;
+    var secTime = 0;
+    var interval;
     var start = document.getElementById("start-timer");
     var stop = document.getElementById("stop-timer");
     var reset = document.getElementById("reset-timer");
     var mins = document.getElementById("minutes");
     var secs = document.getElementById("seconds");
+    var overall = document.getElementById("overallTime");
 
     // Elements
 
     const clickCounter = document.getElementById("click-counter");
     const clickButton = document.getElementById("click-btn");
-
+    const welcome = document.getElementById("welcome-user");
     //reference to variables
     var startTimer = null;
 
@@ -20,6 +23,16 @@ document.addEventListener("DOMContentLoaded", function () {
        clickCounter.textContent = `Clicks: ${clickCount}`;
    });
 
+   welcome.addEventListener("click", function(){
+     let txt;
+     let user = prompt("What is your name?");
+     if (user == null || user == "") {
+       txt = "Astronaut's Idle Clicker";
+     } else {
+       txt = user + "'s Idle Clicker";
+     }
+     document.getElementById("name-tag").textContent = txt;
+   });
    start.addEventListener('click', function(){
        //initialize the variable
        function goTime(){
@@ -27,7 +40,15 @@ document.addEventListener("DOMContentLoaded", function () {
                time();
            }, 1000);
        }
-   goTime();
+    if(mins.value == 0 && secs.value == 0){
+        alert("Please select your time.");
+        stop.hidden = true;
+        start.hidden = false;
+        clickButton.hidden = true;
+      }
+        else{
+          goTime();
+        }
 });
 
 stop.addEventListener("click", function(){
@@ -48,13 +69,17 @@ function time(){
    if(mins.value == 0 && secs.value == 0){
        mins.value = 0;
        secs.value = 0;
+       clickButton.hidden = true;
+       clickCounter.textContent = `Congratulations! You've clicked ${clickCount} times.`;
    }
    else if(secs.value != 0){
        secs.value--;
+       clickButton.hidden = false;
    }
    else if(mins.value != 0 && secs.value == 0){
        secs.value = 59;
        mins.value--;
+       clickButton.hidden = false;
      }
    return;
 }
@@ -63,7 +88,45 @@ function time(){
 //so the time wont go down when selecting a new time after pressing reset
 function stopInterval() {
    clearInterval(startTimer);
+   clickCount = 0;
 }
+
+window.onload = function secretTimer(){
+  if(interval){
+      clearInterval(interval);
+    }
+    interval = setInterval(() => {
+      secTime += 1
+      overall.innerHTML =
+        Math.floor(secTime / 3600).toString().padStart(2, "0") + ":" + Math.floor((secTime % 3600) / 60).toString().padStart(2, "0") + ":" + Math.floor((secTime % 60)).toString().padStart(2, "0")
+    }, 1000);
+}
+fetch('read.json')
+        .then(response => response.json())
+        .then(data => {
+            const dataDisplay = document.getElementById("displayInfo");
+
+            // Create HTML elements to display the JSON data
+            const nameElement = document.createElement("p");
+            nameElement.textContent = "Name: " + data.name;
+
+            const hrsElement = document.createElement("p");
+            hrsElement.textContent = "hours spent: " + data.hoursSpent;
+
+            const clickElement = document.createElement("p");
+            clickElement.textContent = "Clicks: " + data.click;
+
+            // Append the elements to the "dataDisplay" div
+            dataDisplay.appendChild(nameElement);
+            dataDisplay.appendChild(hrsElement);
+            dataDisplay.appendChild(clickElement);
+        })
+        .catch(error => console.error("Error fetching JSON data:", error));
+
+  window.addEventListener("beforeunload", function () {
+     clearInterval(startTime);
+  });
+});
 
 
   window.addEventListener("beforeunload", function () {
